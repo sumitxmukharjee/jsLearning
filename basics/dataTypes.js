d8951975b5d2bd97407f7c13540bbcbdc311f8d3
Sumit Mukharjee <sumitxmukharjee@gmail.com>
2024-06-04 23:32:32 +0530

    Data Type Learned

---
+++ basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using NOdeJs not broweser
+
+// not good
+console.log(3 
+    + 3); //Code readablity should Be high
+console.log("Hitesh");
+
+
+
+let name = "Sumit" //String
+let age = 22 //Number
+let isLoggedIn = false  //Boolean
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof(age))
+console.log(typeof null)  // undefined
+console.log(typeof undefined)  // object 
