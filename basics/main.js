5fb8d45f9a46389b67727ac3a281f7014c10be98
Sumit Mukharjee <sumitxmukharjee@gmail.com>
2024-06-04 22:41:52 +0530

    Learning JS

---
+++ basics/main.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail = "sumit@microsoft.com"
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+
+// accountId = 2
+
+// console.log(accountId) // not allowed
+
+accountEmail = "sumit@yahoo.com"
+accountPassword = "212121"
+accountCity = "Gurugram"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword,accountCity])
