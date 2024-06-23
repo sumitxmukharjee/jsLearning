9f42f0e49a8b5972f773ef41be170c4a58574310
Sumit Mukharjee <sumitxmukharjee@gmail.com>
2024-06-23 22:54:02 +0530

    Learned Conversion

--- basics/conversions.js
+++ basics/conversions.js
@@ -1,8 +1,34 @@
-let score = "33"
+// let score = "33"
+// let score2 = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber = Number(score)
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber2)
+
+
+// "33" -> 33
+// "33abc" => NaN
+// true = 1 or 1 in false
+
+
+
+// let isLoggedIn = ""
+// // let isLoggedIn = "Sumit"  true
+// // let isLoggedIn = 1  true
+// // let isLoggedIn = 0  false
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 34;
+console.log(typeof(someNumber)); //it;s number
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber)) //it's string
