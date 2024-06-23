4dfce8eb254a0777f756bab0a04b481c9ffb492d
Sumit Mukharjee <sumitxmukharjee@gmail.com>
2024-06-23 23:13:52 +0530

    Learned Conversion

--- basics/conversions.js
+++ basics/conversions.js
@@ -4,18 +4,14 @@
 // console.log(typeof score);
 // console.log(typeof(score));
 
-
 // let valueInNumber = Number(score)
 // let valueInNumber2 = Number(score2)
 // console.log(typeof valueInNumber)
 // console.log(typeof valueInNumber2)
 
-
 // "33" -> 33
 // "33abc" => NaN
 // true = 1 or 1 in false
-
-
 
 // let isLoggedIn = ""
 // // let isLoggedIn = "Sumit"  true
@@ -25,10 +21,50 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn)
 
-let someNumber = 34;
-console.log(typeof(someNumber)); //it;s number
+// let someNumber = 34;
+// console.log(typeof(someNumber)); //it;s number
 
-let stringNumber = String(someNumber);
+// let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof(stringNumber)) //it's string
+// console.log(stringNumber);
+// console.log(typeof(stringNumber)) //it's string
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@  Operations @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// let str1 = "Hello "
+// let str2 = "Sumit"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+// tricky behavious doesn';t work
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 3+3;
+
+// let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter)
